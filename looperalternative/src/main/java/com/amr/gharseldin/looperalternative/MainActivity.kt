968e5ee6aaa3c9b9cdce38cfd4a6c032ec683e1a
gharseldin <gharseldin@aucegypt.edu>
2020-09-27 01:20:39 -0700

    Sample Manual Looper application
    -Created a sample looper application without the use of the internal Looper class

---
+++ looperalternative/src/main/java/com/amr/gharseldin/looperalternative/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.amr.gharseldin.looperalternative
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val worker = SimpleWorker()
+    private val handler = Handler(Looper.getMainLooper()){
+        tvMessage.text = it.obj.toString()
+        true
+    }
+    private lateinit var tvMessage: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tvMessage = message
+        worker.excute { ->
+            Thread.sleep(1000)
+            val message = Message.obtain()
+            message.obj = "Task 1 completed"
+            handler.sendMessage(message)
+        }.excute {
+            Thread.sleep(5000)
+            val message = Message.obtain()
+            message.obj = "Task 2 completed"
+            handler.sendMessage(message)
+        }.excute {
+            Thread.sleep(1000)
+            val message = Message.obtain()
+            message.obj = "Task 3 completed"
+            handler.sendMessage(message)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        worker.quit()
+    }
+}
---
+++ looperalternative/src/main/java/com/amr/gharseldin/looperalternative/SimpleWorker.kt
@@ -0,0 +1,31 @@
+package com.amr.gharseldin.looperalternative
+
+import android.util.Log
+import java.util.concurrent.ConcurrentLinkedDeque
+import java.util.concurrent.atomic.AtomicBoolean
+
+private const val TAG = "SimpleWorker"
+class SimpleWorker: Thread(TAG) {
+
+    private val alive = AtomicBoolean(true)
+    private val taskQueue = ConcurrentLinkedDeque<Runnable>()
+
+    init{
+        start()
+    }
+
+    override fun run() {
+        while(alive.get())
+            taskQueue.poll()?.run()
+        Log.i(TAG, "SimpleWorker Terminated")
+    }
+
+    fun excute(task: Runnable):SimpleWorker{
+        taskQueue.add(task)
+        return this
+    }
+
+    fun quit(){
+        alive.set(false)
+    }
+}
